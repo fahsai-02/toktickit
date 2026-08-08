4940c3369cb754e48d198858fe1747c0a399c4e8
fahsai-02 <fahsai.nsf@gmail.com>
2026-08-08 22:09:20 +0700

    test(all): configure Vitest for frontend and Supertest for backend #8 chore(env): add .env.example #11

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,7 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import app from './app.js';
 
-dotenv.config();
+const PORT = process.env.PORT || 5000;
 
-const app = express();
-const port = process.env.PORT || 5000;
-
-app.use(express.json());
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
